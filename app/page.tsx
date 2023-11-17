127e2e4ed7b5e6d11dbef1b0a3bbf709ae30f237
Rajat Verma <rajat311201@gmail.com>
2023-11-17 20:04:51 +0530

    products list

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Billboard } from "@/components/billboard";
 import { Container } from "@/components/container";
+import { ProductsList } from "@/components/products/products-list";
 import { getBillboard, getProducts } from "@/utils/api";
 
 const HomePage = async () => {
@@ -11,7 +12,7 @@ const HomePage = async () => {
             <div className="space-y-10 pb-10">
                 <Billboard data={billboard} />
                 <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    {/* <ProductList title="Featured Products" items={products} /> */}
+                    <ProductsList title="Featured Products" items={products} />
                 </div>
             </div>
         </Container>
