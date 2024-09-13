9e90ea64142c11e9d9b3617c370755790afdf63e
Rajat Verma <rajat311201@gmail.com>
2024-09-13 22:50:41 +0530

    carousel animation

--- components/billboards-carousel.tsx
+++ components/billboards-carousel.tsx
@@ -2,12 +2,16 @@
 
 import {
     Carousel,
+    CarouselApi,
     CarouselContent,
     CarouselItem,
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
 import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 import { Billboard } from "types";
 import { Billboard as BillboardComp } from "./billboard";
 
@@ -18,14 +22,33 @@ interface BillboardsCarouselProps {
 export const BillboardsCarousel: React.FC<BillboardsCarouselProps> = ({
     billboards,
 }) => {
+    const [api, setApi] = useState<CarouselApi>();
+    const [current, setCurrent] = useState(0);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        if (!api) {
+            return;
+        }
+
+        setCount(api.scrollSnapList().length);
+        setCurrent(api.selectedScrollSnap() + 1);
+
+        api.on("select", () => {
+            setCurrent(api.selectedScrollSnap() + 1);
+        });
+    }, [api]);
+
     return (
         <>
             <Carousel
                 plugins={[
                     Autoplay({
-                        delay: 1000,
+                        delay: 1500,
                     }),
                 ]}
+                setApi={setApi}
+                opts={{ loop: true }}
             >
                 <CarouselContent>
                     {billboards.map((billboard) => (
@@ -37,6 +60,28 @@ export const BillboardsCarousel: React.FC<BillboardsCarouselProps> = ({
                 <CarouselPrevious />
                 <CarouselNext />
             </Carousel>
+
+            <div className="flex items-center justify-center gap-5 transition-all duration-200">
+                {billboards.map((item, idx) => {
+                    return (
+                        <>
+                            <Image
+                                key={item.id}
+                                src={item.imageUrl}
+                                height={200}
+                                width={200}
+                                className={cn(
+                                    "h-20 w-32 cursor-pointer rounded-lg border-2 border-transparent object-cover opacity-75 duration-200",
+                                    current === idx + 1 &&
+                                        "scale-110 border-primary/50 opacity-100",
+                                )}
+                                alt="label"
+                                onClick={() => api?.scrollTo(idx)}
+                            />
+                        </>
+                    );
+                })}
+            </div>
         </>
     );
 };
